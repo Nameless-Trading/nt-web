01fcf28901b87413d83348c5d9117672b057b0cd
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-10-02 13:42:48 -0600

    Small changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ import Markets from "@/components/Markets";
 
 //temporary spaghetti code
 async function getGameDays(): Promise<string[]> {
-  const base =
-    process.env.NEXT_PUBLIC_FASTAPI_HTTP ||
-    process.env.FASTAPI_HTTP ||
-    "http://localhost:8000";
+  const base = process.env.NEXT_PUBLIC_FASTAPI_HTTP
   if (!base) throw new Error("FASTAPI base URL is not configured");
+
   const res = await fetch(`${base}/game-days`, { cache: "no-store" });
   if (!res.ok) throw new Error("Failed to load game days");
+  
   return res.json();
 }
 
